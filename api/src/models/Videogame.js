8c22d0590878cc8a3b10ef162749cf794d4d0861
Gaston Garcia Juri <gastongarciajuri@gmail.com>
2022-09-05 14:18:44 -0300

    check rutas

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdinDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,111 @@
-const { Router } = require('express');
+require("dotenv").config();
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require("axios");
+// const Videogame = require('../models/Videogame');
+// const Genres = require('../models/Genres');
+const { Videogame, Genres } = require("../db");
+const { YOUR_API_KEY } = process.env;
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+const getApiInfo = async () => {
+  const apiUrl = await axios.get(
+    `https://api.rawg.io/api/games?key=${YOUR_API_KEY}`
+  );
+  const apiInfo = await apiUrl.data.results.map((e) => {
+    return {
+      image: e.background_image,
+      name: e.name,
+      genres: e.genres.map((x) => x),
+      description: e.description,
+      rating: e.rating,
+      platforms: e.platforms.map((x) => x),
+      id: e.id,
+    };
+  });
+  return apiInfo;
+};
 
+const getDbInfo = async () => {
+  return await Videogame.findAll({
+    include: {
+      model: Genres,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+};
+
+const getAllGames = async () => {
+  const apiInfo = await getApiInfo();
+  const dbInfo = await getDbInfo();
+  const infoTotal = apiInfo.concat(dbInfo);
+  return infoTotal;
+};
+
+router.get("/videogame", async (req, res) => {
+  try {
+    const name = req.query.name;
+    let gameTotal = await getAllGames();
+    if (name) {
+      let gameName = await gameTotal.filter((e) =>
+        e.name.toLowerCase().includes(name.toLowerCase())
+      );
+      if (gameName.length) {
+        if (gameName.length > 15) {
+          gameName.slice(0, 14);
+          res.status(200).json(gameName);
+        }
+        res.status(200).json(gameName);
+      }
+      res.status(404).send(`No hay nada sobre ${name}`);
+    } else {
+      res.status(200).send(gameTotal);
+    }    
+  } catch (error) {
+    res.status(404).json(error)
+  }
+});
+router.get("/genres", async (req, res) => {
+  //no funciona
+  try {
+    let genresFinal = [];
+    const genresApi = await axios.get(
+      `https://api.rawg.io/api/games?key=${YOUR_API_KEY}`
+    );
+    const genres = genresApi.data.results.map((e) =>
+      e.genres.map((x) => x.name)
+    );
+    //console.log(genres, "genres")
+    const genresEach = genres.flat();
+    //console.log(genresEach, "genreich")
+    genresEach.forEach(async (e) => {
+      if (genresFinal.includes(e)) {
+        return;
+      } else {
+        genresFinal.push(e);
+      }
+    });
+    console.log(genresFinal);
+    genresFinal.forEach(async (el) => {
+      await Genres.findOrCreate({
+        where: { name: el },
+        defaults: { name: el },
+      });
+    });
+    //console.log(genresEach, "esto es genreEach")
+    const allGenres = await Genres.findAll();
+    res.json(allGenres);
+  } catch (error) {
+    res.status(404).json(error);
+    console.log(error);
+  }
+});
 module.exports = router;
